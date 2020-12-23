89ce10e43599f412d1e3bf2bcc55992866d88da2
Justas <justas.poderys@gmail.com>
2020-12-23 13:35:56 +0200

    Adding a sample GtkSharp project

---
+++ SikGUIGtk/Category.cs
@@ -0,0 +1,12 @@
+// This is free and unencumbered software released into the public domain.
+// Happy coding!!! - GtkSharp Team
+
+namespace Samples
+{
+    enum Category
+    {
+        Widgets,
+        Dialogs,
+        Miscellaneous
+    }
+}
